6906d9503039c7f61ad32cf5d2bb368a39f36126
Pozdeev-Igor <71305891+Pozdeev-Igor@users.noreply.github.com>
2021-08-22 10:48:16 +0300

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -14,9 +14,9 @@ public class BubbleSort {
         for (int i = 0; i < nums.length-1; i++) {
            for (int j = i+1; j < nums.length; j++) {
                if (Integer.parseInt(nums[i]) < Integer.parseInt(nums[j])) {
-                   a = nums[i];
-                   nums[i] = nums[j];
-                   nums[j] = a;
+                         a  = nums[i];
+                   nums[i]  = nums[j];
+                   nums[j]  = a;
                }
            }
        }
